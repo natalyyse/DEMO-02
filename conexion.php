b8b35542aeba757fbacecfd9e1dc0060f4734635
natalyyse <147267756+natalyyse@users.noreply.github.com>
2024-08-26 17:25:58 -0500

    update2

--- conexion.php
+++ conexion.php
@@ -1,11 +1,11 @@
 <?php
 	function conexion(){
 
-	$host = "host=viaduct.proxy.rlwy.net";
-	$port = "port=26862";
-	$dbname = "dbname=railway";
-	$user = "user=postgres";
-	$password = "password=SIIZpYZuHrHzKtzfUhAFNdDKKPEACBSJ";
+	$host = "host=dpg-cr6g0756l47c73f3hf5g-a";
+	$port = "port=5432";
+	$dbname = "dbname=dbprueba_38s2";
+	$user = "user=dbprueba_38s2_user";
+	$password = "password=bjafkZRG5BuQSM2Is6bNYuCQUC8XyfYE";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
